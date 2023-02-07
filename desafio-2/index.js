905a339d21fbde24301a08d9c32fdfc09aaf5ba1
Julian D. Abregu <julianabregupc@gmail.com>
2023-02-07 04:26:09 -0300

    correcciones desafio 2

--- desafio-2/index.js
+++ desafio-2/index.js
@@ -43,7 +43,7 @@ class ProductManager {
   getProductById = async (id) => {
     let content = await this.readProducts();
     if (!content.find((product) => product.id === id)) {
-      console.log("Id not found");
+      console.log("[ERROR] Id not found");
     } else {
       console.log(content.find((product) => product.id === id));
     }
@@ -53,62 +53,82 @@ class ProductManager {
     let content = await this.readProducts();
     let deleteById = content.filter((product) => product.id != id);
 
-    await fs.writeFile(this.path, JSON.stringify(deleteById));
-    console.log(`product ${id} has been deleted.`);
+    if (!content.find((product) => product.id === id)) {
+      console.log(`[ERROR] Product ${id} doesn't exist`);
+    } else {
+      await fs.writeFile(this.path, JSON.stringify(deleteById));
+    }
   };
 
-  updateProduct = async ({ id, ...title }) => {
-    await this.deleteProductById(id);
+  updateProduct = async ({
+    title,
+    description,
+    price,
+    thumbnail,
+    code,
+    stock,
+    id,
+  }) => {
     let content = await this.readProducts();
 
-    let updateProd = [{ ...title, id }, ...content];
-
-    await fs.writeFile(this.path, JSON.stringify(updateProd));
-    console.log(`product ${id} has been replaced.`);
+    if (content.some((product) => product.id === id)) {
+      let index = content.findIndex((product) => product.id == id);
+      content[index].title = title;
+      content[index].description = description;
+      content[index].price = price;
+      content[index].thumbnail = thumbnail;
+      content[index].code = code;
+      content[index].stock = stock;
+      await fs.writeFile(this.path, JSON.stringify(content));
+    } else {
+      console.log("[ERROR] Product id is not valid.");
+    }
   };
 }
 
 const productManager = new ProductManager("./products.txt");
 
-await productManager.addProduct(
-  "Salomon shoe",
-  "Outdoor shoes",
-  42399,
-  "img-1",
-  "HSK27X",
-  6
-);
+// await productManager.getProducts();
 
-await productManager.addProduct(
-  "Nike t-shirt",
-  "Running clothes",
-  9899,
-  "img-2",
-  "HSKXCX",
-  12
-);
+// await productManager.addProduct(
+//   "Salomon shoe",
+//   "Outdoor shoes",
+//   42399,
+//   "img-1",
+//   "HSK27X",
+//   6
+// );
 
-await productManager.addProduct(
-  "Puma shoe",
-  "Running shoes",
-  47999,
-  "img-3",
-  "DSJF2B",
-  8
-);
+// await productManager.addProduct(
+//   "Nike t-shirt",
+//   "Running clothes",
+//   9899,
+//   "img-2",
+//   "HSKXCX",
+//   12
+// );
+
+// await productManager.addProduct(
+//   "Puma shoe",
+//   "Running shoes",
+//   47999,
+//   "img-3",
+//   "DSJF2B",
+//   8
+// );
 
 // await productManager.getProducts();
-// await productManager.getProductById(1)
-// await productManager.deleteProductById(1);
+// await productManager.getProductById(2);
+// await productManager.deleteProductById(3);
 // await productManager.getProducts();
 // await productManager.updateProduct({
-//   id: 3,
+//   id: 1,
 //   title: "Vans",
 //   description: "Urban Shoes",
-//   price: 15000,
+//   price: 20000,
 //   thumbnail: "img-9",
 //   code: "2KMVC3",
 //   stock: 30,
 // });
-// await productManager.getProductById(3);
+// await productManager.getProductById(1);
 // await productManager.getProducts();
